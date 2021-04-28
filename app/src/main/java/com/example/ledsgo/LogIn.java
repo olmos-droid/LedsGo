f0caab498f0dafed26a7813b952b619af4e4df3f
Josep Mª Olmos <56126978+olmos-droid@users.noreply.github.com>
2021-04-28 02:08:55 +0200

    colors.xml modificats
    pantalla login feta amb tipografia i botons i tamany de lletra
    
    cambiats el backgroundtint dels textedit que es la linea que els limita per sota
    
     tot esta @strings per la traduccio
    
     ic_launc (icona de la app feta)
    
     TODO TRADUCCIO I ORTOGRAFAI

--- app/src/main/java/com/example/ledsgo/LogIn.java
+++ app/src/main/java/com/example/ledsgo/LogIn.java
@@ -2,13 +2,33 @@ package com.example.ledsgo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LogIn extends AppCompatActivity {
+
+    Button buttonSignUP;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+buttonSignUP = findViewById(R.id.button_Login_CreateUser);
+
+
+
+buttonSignUP.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(getApplicationContext(),SignUp.class);
+        startActivity(intent);
+    }
+});
+
     }
 }
