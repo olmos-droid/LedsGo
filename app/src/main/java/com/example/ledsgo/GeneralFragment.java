bfb3828d50a5c1cf4b3c3f79d227389b6627d144
Josep Mª Olmos <56126978+olmos-droid@users.noreply.github.com>
2021-04-29 22:54:49 +0200

    frmaworks fets, falta omplirlos amb els botons de paterns

---
+++ app/src/main/java/com/example/ledsgo/GeneralFragment.java
@@ -0,0 +1,23 @@
+package com.example.ledsgo;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class GeneralFragment extends Fragment {
+    public GeneralFragment(){
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_general, container, false);
+    }
+}
--- app/src/main/java/com/example/ledsgo/Show.java
+++ app/src/main/java/com/example/ledsgo/Show.java
@@ -1,14 +1,68 @@
 package com.example.ledsgo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Show extends AppCompatActivity {
+    private BottomNavigationView mMainNav;
+    private FrameLayout mMainFrame;
+    private GeneralFragment generalFragment;
+    private GroupsFragment groupsFragment;
+    private StripsFragment stripsFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show);
+
+        mMainFrame = findViewById(R.id.main_frame);
+        mMainNav = findViewById(R.id.main_nav);
+
+        generalFragment = new GeneralFragment();
+        groupsFragment = new GroupsFragment();
+        stripsFragment = new StripsFragment();
+
+        mMainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.nav_menu_general:
+                        //open the HomeFragment
+                        //We can change the color of the background using the following:
+                        mMainNav.setItemBackgroundResource(R.color.purple_100);
+                        setFragment(generalFragment);
+                        return true;
+                    case R.id.nav_menu_strips:
+                        //open the ProfileFragment
+                        mMainNav.setItemBackgroundResource(R.color.blue_100);
+                        setFragment(stripsFragment);
+                        return true;
+                    case R.id.nav_menu_groups:
+                        //open the SettingsFragment
+                        mMainNav.setItemBackgroundResource(R.color.orange_100);
+                        setFragment(groupsFragment);
+                        return true;
+                    default:
+                        return false;
+                }
+
+            }
+        });
+    }
+    private void setFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame, fragment);
+        fragmentTransaction.commit();
     }
 }
